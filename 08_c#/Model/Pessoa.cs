7bcaff50e22ad0b597ffdc5a5d13f90109348502
shishiv <myke.matos@gmail.com>
2025-11-10 18:06:52 -0300

    reorganized repo

--- 08_c#/Model/Pessoa.cs
+++
@@ -1,9 +0,0 @@
-namespace BibliotecaJK.Model
-{
-    public abstract class Pessoa
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; } = string.Empty;
-        public string CPF { get; set; } = string.Empty;
-    }
-}
